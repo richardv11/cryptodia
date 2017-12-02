35d8b6f91bb2b0ff3ee63014bdc8f61e43a3b28a
Richard Vacas <richardvacas@live.com>
2017-12-02 18:46:10 -0500

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package a477.cryptodia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
